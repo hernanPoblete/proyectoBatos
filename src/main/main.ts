0905d216c4d524a9089baf647b45e03ab17af7ea
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2024-11-11 12:50:57 -0300

    fix(*): Actually adds src as promised by commit 9a8869bcf271097e26c27a6a2b92f2f93af2f8fe

---
+++ src/main/main.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+import {join} from 'path';
+
+
+/**
+ * Settings for the environment vars. Provides security
+ * by not exposing directly credentials
+ */
+
+dotenv.config({
+    path: join("./", ".env")
+});
+
+/**
+ * Creates an express app for the project
+ */
+const app = express();
+const port: Number = parseInt(process.env.PORT!);
+
+/**
+ * Sets the view engine to ejs
+ */
+app.set('view engine', require('ejs'));
+
+/**
+ * Configures the views directory to be 
+ * src/views 
+ */
+app.set('views', join('src', 'main', 'views'));
+
+/**
+ * Configures the main request of the app to
+ * render and then display the content inside
+ * index.ejs
+ */
+app.get("/", (req: Request, res: Response)=>{
+    res.status(200).render('index.ejs', {
+        components: ["./components/HelloWorld/content.ejs"]
+    });
+});
+
+
+/**
+ * configures request for paths 
+ * not found within the app
+ */
+app.get("*", (req: Request, res: Response)=>{
+    res.status(404).send("OHOH. Pagina No encontrada");
+})
+
+/**
+ * Runs the app.
+ * Once the app is listening on set port
+ * prints a message
+ */
+app.listen(port, ()=>{
+    console.log(`App Listening in port ${port}`);
+});
